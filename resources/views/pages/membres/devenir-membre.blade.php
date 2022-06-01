134f1cf42c39dd424c6a65a196bf3526a4b9ad2d
Jean-Luc Mupasa <0997699393jm@gmail.com>
2022-06-01 02:36:29 -0700

    refactoring form register member

--- resources/views/pages/membres/devenir-membre.blade.php
+++ resources/views/pages/membres/devenir-membre.blade.php
@@ -27,7 +27,7 @@
     <div class="login-area default-padding">
         <div class="container">
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
+                <div class="col-md-12 ">
                     <form method="post" action="{{ route('members.register') }}" id="register-form" class="white-popup-block">
 
                         @csrf
@@ -56,15 +56,11 @@
                             <h4>Faites partie des Leaders, en devenant membre.</h4>
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="nomcomplet">Nom complet</label>
                                         <input class="form-control" name="nomcomplet" id="nomcomplet" type="text">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="email">Addresse email</label>
                                         <input class="form-control" name="email" id="email" type="email">
                                     </div>
@@ -72,15 +68,11 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="profession">Profession</label>
                                         <input class="form-control" name="profession" type="text" id="profession">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="age">Age</label>
                                         <input class="form-control" name="age" type="number" id="age">
                                     </div>
@@ -88,10 +80,14 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
+                                        <label for="username">Nom d'utilisateur</label>
+                                        <input class="form-control" name="username" id="username" type="text">
+                                    </div>
+                                    <div class="form-group col-md-6">
                                         <label for="sexe">Sexe</label>
                                         <select class="form-control" name="sexe" id="sexe">
-                                            <option value="">---------------------------------- Sélectionner votre genre ----------------------------------</option>
+                                            {{--                                            <option value="">Sélectionner votre genre</option>--}}
                                             <option value="Homme">Homme</option>
                                             <option value="Femme">Femme</option>
                                         </select>
@@ -100,23 +96,11 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="row">
-                                    <div class="form-group">
-                                        <label for="username">Nom d'utilisateur</label>
-                                        <input class="form-control" name="username" id="username" type="text">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="password">Mot de passe</label>
                                         <input class="form-control" name="password" type="password" id="password">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label for="confirm_password">Confirmer mot de passe</label>
                                         <input class="form-control" name="confirm_password" type="password">
                                     </div>
